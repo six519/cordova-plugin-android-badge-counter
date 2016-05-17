eb61721a960dc4fa15451fdb8283d8f70acfca30
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-05-17 12:46:28 +0800

    set javascript interface

--- www/badgecounterplugin.js
+++ www/badgecounterplugin.js
@@ -3,6 +3,12 @@
 var exec = require("cordova/exec");
 
 var badgeCounterPlugin = {
+	setBadge: function(badge_count, sc, ec) {
+		exec(sc, ec, "BadgeCounterPlugin", "login", [badge_count]);
+	},
+	clearBadge: function(sc, ec) {
+		exec(sc, ec, "BadgeCounterPlugin", "clearBadge", []);	
+	}
 };
 
 module.exports = badgeCounterPlugin;
