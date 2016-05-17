fc6337445a967e9bf24e61fa43c69ab0926af846
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-05-18 02:01:35 +0800

    test get id

--- scripts/afterInstall.js
+++ scripts/afterInstall.js
@@ -7,6 +7,21 @@ module.exports = function(context) {
         var xml2js = require('xml2js');
     }catch(e) {
         exec("npm install xml2js");
+        var xml2js = require('xml2js');
     }
+    
+    var json;
+    try {
+        var configFile = fs.readFileSync('config.xml', 'ascii');
+        var parser = new xml2js.Parser();
+        parser.parseString(configFile.substring(0, configFile.length), function (err, result) {
+            json = result;
+        });
+    } catch (e) {
+        console.log(e)
+    }
+    
+    var cordova_app_id = json.widget.$.id;
+    console.log('The id is: ' + cordova_app_id);
 
 };
